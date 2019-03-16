e9d008832e0c7c80d7e736035f5e42b549ad04aa
Jiarong Gu <jiaronggu@hotmail.com>
2019-03-16 12:22:41 +1100

    update local content extraction

--- src/cssTypeHelper.ts
+++ src/cssTypeHelper.ts
@@ -2,19 +2,25 @@ import * as path from 'path';
 import { reservedWords } from './reservedWords';
 
 export function getCssModuleKeys(content) {
-  const cssKeys: Array<string> = [];
-  const keyRegex = /"(?<key>[^\\"]+)":/g;
+  // get local exports
+  const localRegex = /locals\s*=\s*{(?<content>[\s\S]*)}/g;
+  const localContent = getGroupedMatches(localRegex, content, 'content').join('\n');
 
   // get all css keys from content
+  const keyRegex = /"(?<key>[^\\"]+)":/g;
+  const cssKeys = getGroupedMatches(keyRegex, localContent, 'key');
+  return cssKeys.sort();
+}
+
+function getGroupedMatches(regex: RegExp, content: string, group: string) {
+  const matches: Array<string> = [] = [];
   let match: RegExpExecArray | null;
   do {
-    match = keyRegex.exec(content);
-    if (match && match.groups && match.groups.key)
-      cssKeys.push(match.groups.key);
+    match = regex.exec(content);
+    if (match && match.groups && match.groups[group])
+      matches.push(match.groups[group]);
   } while (match != null)
-
-  // filter out none ts formats
-  return cssKeys.sort();
+  return matches;
 }
 
 export function filterNonWordKeys(cssModuleKeys) {
